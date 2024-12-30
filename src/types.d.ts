ccc629ea6fa629dffb51a6f7178cd8b7609ca44d
shahrul-azhar <110072608+shahrul-azhar@users.noreply.github.com>
2024-12-30 16:08:52 +0800

    Added obj upload, delete, get, list endpoints for R2 integration (#1)
    Co-authored-by: shahrul <shahrul@cloudflare.com>

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,8 @@
 import { Context } from "hono";
 
 type Bindings = {
-    DB: D1Database;
-}
+  DB: D1Database;
+  R2_BUCKET: blazenote;
+};
 
-export type ContextExtended = Context<{ Bindings: Bindings }>
+export type ContextExtended = Context<{ Bindings: Bindings }>;
