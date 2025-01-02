e6cbca1fdda3fb23d73c189ff9c92d3fec5cd0d4
shahrul <shahrul@cloudflare.com>
2025-01-02 09:51:22 +0800

    Added pre signed url endpoint

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,6 +3,8 @@ import { Context } from "hono";
 type Bindings = {
   DB: D1Database;
   R2_BUCKET: blazenote;
+  R2_ACCESS_KEY: string;
+  R2_SECRET_KEY: string;
 };
 
 export type ContextExtended = Context<{ Bindings: Bindings }>;
