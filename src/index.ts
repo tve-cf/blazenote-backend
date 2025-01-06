310f9a04cc226f0fbb57b3a6e961656a027d7c01
Nurul Zaman <hanisahkzdev@gmail.com>
2025-01-06 15:56:41 +0800

    Fix CORS issue and update implementation in notes routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import notes from './routes/notes.route'
 import files from './routes/files.route'
 
 const app = new Hono()
+// https://hono.dev/docs/middleware/builtin/cors
+app.use(
+  cors({
+    origin: ['http://localhost:5173'], // TODO: To add prod origin
+    allowMethods: ['POST', 'GET', 'DELETE', 'PUT'],
+    maxAge: 600,
+  })
+)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
