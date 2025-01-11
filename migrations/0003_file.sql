8b7a5d97cf508ba4af681b969c7e137999a76cb1
shahrul-azhar <110072608+shahrul-azhar@users.noreply.github.com>
2025-01-11 09:47:31 +0800

    Add deletion route (#6)
    * Single upload working with file table  updated
    
    * Add list of route
    
    * Add deletion route
    
    ---------
    
    Co-authored-by: shahrul <shahrul@cloudflare.com>

---
+++ migrations/0003_file.sql
@@ -0,0 +1,5 @@
+-- Migration number: 0003 	 2025-01-09T08:24:51.363Z
+ALTER TABLE
+  "file"
+DROP COLUMN
+  "url";
--- src/routes/files.route.ts
+++ src/routes/files.route.ts
@@ -4,185 +4,11 @@ import {
   S3Client,
   PutObjectCommand,
   GetObjectCommand,
+  DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const files = new Hono<ContextExtended>();
-
-// Route to get all files (empty list for now)
-/*files.get("/", (ctx) => {
-  return ctx.json([]);
-});
-*/
-
-// Route to list all files in the bucket
-files.get("/list", async (ctx) => {
-  const bucket = ctx.env.R2_BUCKET;
-
-  try {
-    const objects = await bucket.list();
-    //console.log(objects); // Log the raw response
-    const keys = objects.objects.map((object) => object.key);
-    return ctx.json({ success: true, keys });
-  } catch (error) {
-    return ctx.json({
-      success: false,
-      message: "Error listing objects",
-      error: error.message,
-    });
-  }
-});
-
-// Route to upload a file
-files.post("/upload", async (ctx) => {
-  const formData = await ctx.req.formData();
-  const file = formData.get("file"); // Assuming 'file' is the key in the form
-
-  if (!file) {
-    return ctx.json({ success: false, message: "No file uploaded" });
-  }
-
-  // Append timestamp to the file name to ensure uniqueness
-  const timestamp = Date.now();
-  const uniqueFilename = `${timestamp}-${file.name}`;
-
-  try {
-    // Upload the file with the new unique filename
-    await ctx.env.R2_BUCKET.put(uniqueFilename, file.stream(), {
-      httpMetadata: { contentType: file.type },
-    });
-
-    return ctx.json({
-      success: true,
-      message: "File uploaded successfully",
-      filename: uniqueFilename,
-    });
-  } catch (error) {
-    return ctx.json({
-      success: false,
-      message: "Error uploading file",
-      error: error.message,
-    });
-  }
-});
-
-// Route to generate pre-signed url
-files.post("/pre-signed-url", async (ctx) => {
-  const { r2, bucket } = createR2Client(ctx);
-  const key = crypto.randomUUID();
-  const url = await getSignedUrl(
-    r2,
-    new PutObjectCommand({ Bucket: bucket, Key: key })
-  );
-
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "POST",
-    "Access-Control-Allow-Headers": "Content-Type",
-  };
-
-  return ctx.json({ key, url }, { headers });
-});
-
-// Route to delete a specific file by ID
-files.delete("/:id", async (ctx) => {
-  const filename = ctx.req.param("id"); // Use ctx.req.param() to access the URL parameter
-
-  if (!filename) {
-    return ctx.json({
-      success: false,
-      message: "No file key provided",
-    });
-  }
-
-  try {
-    // Attempt to delete the file from the R2 bucket
-    await ctx.env.R2_BUCKET.delete(filename);
-
-    return ctx.json({
-      success: true,
-      message: `File ${filename} deleted successfully`,
-    });
-  } catch (error) {
-    return ctx.json({
-      success: false,
-      message: `Error deleting file ${filename}`,
-      error: error.message,
-    });
-  }
-});
-
-// Route to get a specific file by ID
-/*
-files.get("/:id", async (ctx) => {
-  const filename = ctx.req.param("id"); // Get the file key (ID) from the URL parameter
-
-  if (!filename) {
-    return ctx.json({
-      success: false,
-      message: "No file key provided",
-    });
-  }
-
-  try {
-    // Attempt to retrieve the file from the R2 bucket using the key (filename)
-    const file = await ctx.env.R2_BUCKET.get(filename);
-
-    if (file) {
-      // File exists, return the file content with the appropriate content type
-      return ctx.body(file.body, {
-        headers: { "Content-Type": file.httpMetadata.contentType },
-      });
-    } else {
-      return ctx.json({
-        success: false,
-        message: `File with key ${filename} not found`,
-      });
-    }
-  } catch (error) {
-    return ctx.json({
-      success: false,
-      message: `Error retrieving file ${filename}`,
-      error: error.message,
-    });
-  }
-});
-*/
-
-// Route to get a specific file by key
-files.get("/:key", async (ctx) => {
-  const { r2, bucket } = createR2Client(ctx);
-  const fileKey = ctx.req.param("key");
-
-  if (!fileKey) {
-    return ctx.json({
-      success: false,
-      message: "No file key provided",
-    });
-  }
-
-  try {
-    const url = await getSignedUrl(
-      r2,
-      new GetObjectCommand({ Bucket: bucket, Key: fileKey }),
-      { expiresIn: 3600 }
-    );
-
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      "Access-Control-Allow-Headers": "Content-Type",
-    };
-
-    return ctx.json({ success: true, key: fileKey, url }, { headers });
-  } catch (error) {
-    return ctx.json({
-      success: false,
-      message: `Error generating pre-signed URL for file ${fileKey}`,
-      error: error.message,
-    });
-  }
-});
 
 // Helper to init the s3 client
 function createR2Client(ctx: any) {
@@ -199,5 +25,185 @@ function createR2Client(ctx: any) {
 
   return { r2, bucket };
 }
+
+// Route to get a list of files attached to a note from db
+files.get("/list/:noteId", async (ctx) => {
+  const db = ctx.env.DB;
+  const noteId = ctx.req.param("noteId");
+
+  try {
+    const query = "SELECT id, name FROM file WHERE note_id = ?";
+    const results = await db.prepare(query).bind(noteId).all();
+
+    if (!results.success || results.results.length === 0) {
+      return ctx.json({
+        success: true,
+        names: [],
+        message: "No files found for the specified note id",
+      });
+    }
+
+    const names = results.results.map((row) => ({
+      id: row.id,
+      name: row.name,
+    }));
+
+    return ctx.json({ success: true, names });
+  } catch (error) {
+    return ctx.json({
+      success: false,
+      message: "Error retrieving files",
+      error: error.message,
+    });
+  }
+});
+
+// Route to generate pre-signed url for upload
+files.post("/pre-signed-url", async (ctx) => {
+  try {
+    const { r2, bucket } = createR2Client(ctx);
+    const { fileName } = await ctx.req.json();
+    if (!fileName) {
+      throw new Error("File name is required.");
+    }
+
+    const fileExtension = fileName.split(".").pop();
+    const baseName = fileName.replace(`.${fileExtension}`, "");
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    const key = `${timestamp}-${baseName}.${fileExtension}`;
+
+    const url = await getSignedUrl(
+      r2,
+      new PutObjectCommand({ Bucket: bucket, Key: key })
+    );
+
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Headers": "Content-Type",
+    };
+
+    return ctx.json({ key, url }, { headers });
+  } catch (error) {
+    console.error("Error generating pre-signed URL:", error);
+    return ctx.json(
+      { message: "Failed to generate pre-signed URL", error: error.message },
+      { status: 500 }
+    );
+  }
+});
+
+// Route to generate pre-signed url for download
+files.get("/pre-signed-url/:fileName", async (ctx) => {
+  try {
+    const { r2, bucket } = createR2Client(ctx);
+    const fileName = ctx.req.param("fileName");
+
+    if (!fileName) {
+      throw new Error("File name is required.");
+    }
+
+    const url = await getSignedUrl(
+      r2,
+      new GetObjectCommand({
+        Bucket: bucket,
+        Key: fileName,
+      }),
+      { expiresIn: 900 }
+    );
+
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "Content-Type",
+    };
+
+    return ctx.json({ url }, { headers });
+  } catch (error) {
+    console.error("Error generating pre-signed URL:", error);
+    return ctx.json(
+      { message: "Failed to generate pre-signed URL", error: error.message },
+      { status: 500 }
+    );
+  }
+});
+
+// Route to save files metadata to file table
+files.post("/save", async (ctx) => {
+  try {
+    const { noteId, objectKey } = await ctx.req.json();
+    console.log("Received noteId:", noteId);
+    console.log("Received file key:", objectKey);
+
+    const id = crypto.randomUUID();
+    const db = ctx.env.DB;
+
+    // Get the current Unix timestamp
+    const createdAt = Math.floor(Date.now() / 1000);
+
+    // Insert the file metadata into the 'file' table
+    const response = await db
+      .prepare(
+        `INSERT INTO file (id, note_id, name, created_at, updated_at) 
+         VALUES (?, ?, ?, ?, ?)`
+      )
+      .bind(id, noteId, objectKey, createdAt, createdAt)
+      .run();
+
+    if (response && response.affectedRows > 0) {
+      return ctx.json({ message: "File metadata saved successfully" });
+    } else {
+      return ctx.json({ message: "Failed to save file metadata" });
+    }
+  } catch (error) {
+    console.error(`Failed to save file metadata. Reason: ${error.message}`);
+    return ctx.json({
+      message: `Failed to save file metadata. Reason: ${error.message}`,
+    });
+  }
+});
+
+// Delete attachment based on noteId
+files.delete("/:noteId", async (ctx) => {
+  const db = ctx.env.DB;
+  const noteId = ctx.req.param("noteId");
+
+  try {
+    // Fetch files associated with the noteId
+    const query = "SELECT id, name FROM file WHERE note_id = ?";
+    const results = await db.prepare(query).bind(noteId).all();
+
+    if (!results.success || results.results.length === 0) {
+      return ctx.json({
+        success: false,
+        message: `No files found for: ${noteId}`,
+      });
+    }
+
+    const names = results.results.map((row) => row.name);
+
+    // Parallel deletion of files
+    const deletePromises = names.map((name) =>
+      ctx.env.R2_BUCKET.delete(name).catch((error) => {
+        console.error(`Error deleting file ${name}:`, error.message);
+      })
+    );
+    await Promise.all(deletePromises);
+
+    // Return success response
+    return ctx.json({
+      success: true,
+      message: "All files deleted.",
+    });
+  } catch (error) {
+    // Handle database query or other errors
+    console.error(`Failed to process files for ${noteId}: ${error.message}`);
+    return ctx.json({
+      success: false,
+      message: `Failed to process files for deletion. Reason: ${error.message}`,
+    });
+  }
+});
 
 export default files;
