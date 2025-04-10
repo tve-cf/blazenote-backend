0547f746c493f544a57fbfb13b1f1cee8608f155
shahrul <shahrul@cloudflare.com>
2025-04-10 17:03:38 +0800

    Module 2

--- src/routes/notes.route.ts
+++ src/routes/notes.route.ts
@@ -3,29 +3,68 @@ import { ContextExtended } from "../types";
 
 const notes = new Hono();
 
-// TODO: In-Workshop Activities.
-notes.get('/', async (ctx: ContextExtended) => {
-    return Response.json([])
+notes.get("/", async (ctx: ContextExtended) => {
+  const db = ctx.env.DB;
+  const notes = await db.prepare("SELECT * FROM note LIMIT 50").run();
+
+  return Response.json(notes.results);
 });
 
-// TODO: In-Workshop Activities.
-notes.get('/:id', async (ctx: ContextExtended) => {
-    return Response.json({})
+notes.get("/:id", async (ctx: ContextExtended) => {
+  const id = ctx.req.path.split("/").slice(-1).join();
+  const db = ctx.env.DB;
+  const note = await db
+    .prepare("SELECT * FROM note  WHERE id = ?1")
+    .bind(id)
+    .first();
+
+  return Response.json(note);
 });
 
-// TODO: In-Workshop Activities.
-notes.post('/', async (ctx: ContextExtended) => {
-    return Response.json({ message: "note created" })
+notes.post("/", async (ctx: ContextExtended) => {
+  try {
+    const { id, title, description } = await ctx.req.json();
+    const db = ctx.env.DB;
+    const response = await db
+      .prepare(`INSERT INTO note (id, title, description) VALUES (?1, ?2, ?3)`)
+      .bind(id, title, description)
+      .run();
+
+    return response.success
+      ? Response.json({ message: "note created" })
+      : Response.json({ message: "failed to create note" });
+  } catch (e) {
+    console.error(`failed to create note. reason: ${e}`);
+    return Response.json({ message: `failed to create note. reason: ${e}` });
+  }
 });
 
-// TODO: In-Workshop Activities.
-notes.put('/:id', async (ctx: ContextExtended) => {
-    return Response.json({ message: "note updated" })
-});
+notes.delete("/:id", async (ctx: ContextExtended) => {
+  try {
+    const id = ctx.req.path.split("/").slice(-1).join();
+    const db = ctx.env.DB;
+    const fileResponse = await db
+      .prepare("DELETE FROM file where note_id == ?1")
+      .bind(id)
+      .run();
 
-// TODO: In-Workshop Activities.
-notes.delete('/:id', async (ctx: ContextExtended) => {
-    return Response.json({ message: "note deleted" })
+    if (fileResponse.success) {
+      const noteResponse = await db
+        .prepare("DELETE FROM note where id == ?1")
+        .bind(id)
+        .run();
+
+      return noteResponse.success
+        ? Response.json({ message: "note delete" })
+        : Response.json({ message: "failed to delete note" });
+    } else {
+      console.log("failed to delete note");
+      return Response.json({ message: "failed to delete note" });
+    }
+  } catch (e) {
+    console.error(`failed to delete note. reason: ${e}`);
+    return Response.json({ message: `failed to delete note. reason: ${e}` });
+  }
 });
 
 export default notes;
