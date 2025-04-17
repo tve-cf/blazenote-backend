51204d3fedde851607095dff508c5e6528577c27
Amirul Zharfan Bin Zalid <amirulzharfanbin@cloudflare.com>
2025-04-17 17:13:00 +0800

    Implement initial database migration with note and file tables, including UUID generation and triggers for automatic timestamp updates. Remove previous file table migration and URL column.

--- migrations/0001_initial.sql
+++ migrations/0001_initial.sql
@@ -1,4 +1,4 @@
--- Migration number: 0001 	 2024-12-30T03:52:41.446Z
+-- Migration number: 0001 	 2025-03-26T03:52:41.446Z
 
 DROP VIEW IF EXISTS uuid7;
 CREATE VIEW uuid7 AS
@@ -34,4 +34,30 @@ CREATE TRIGGER trigger_after_insert_on_note
     AFTER INSERT ON note WHEN NEW.id IS NULL
 BEGIN
 	UPDATE note SET id = (SELECT next FROM uuid7) WHERE ROWID = NEW.ROWID;
+END;
+
+
+CREATE TABLE file (
+    id TEXT PRIMARY KEY,                                 -- UUID as a string
+    note_id INTEGER,                                     -- Reference to note id
+    name TEXT NOT NULL,                                  -- File name
+    created_at INTEGER DEFAULT (strftime('%s', 'now')),  -- Unix epoch timestamp for creation (defaults to current time)
+    updated_at INTEGER DEFAULT (strftime('%s', 'now')),  -- Unix epoch timestamp for last update (defaults to current time)
+FOREIGN KEY (note_id) REFERENCES note(id)
+);
+
+-- Trigger to automatically update the `updated_at` field when a record is modified
+CREATE TRIGGER update_file_before_update
+AFTER UPDATE ON file
+FOR EACH ROW
+BEGIN
+    UPDATE file SET updated_at = strftime('%s', 'now') WHERE id = OLD.id;
+END;
+
+-- Create the trigger using built-in SQLite functions
+DROP TRIGGER IF EXISTS trigger_after_insert_on_file;
+CREATE TRIGGER trigger_after_insert_on_file
+    AFTER INSERT ON file WHEN NEW.id IS NULL
+BEGIN
+	UPDATE file SET id = (SELECT next FROM uuid7) WHERE ROWID = NEW.ROWID;
 END;
