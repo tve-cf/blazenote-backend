7d87c0383ecb2d48eb1cd9282c0caee921b30bc0
Amirul Zharfan Bin Zalid <amirulzharfanbin@cloudflare.com>
2025-05-26 23:40:26 +0800

    Fix image

--- src/routes/images.route.ts
+++ src/routes/images.route.ts
@@ -3,22 +3,17 @@ import { ContextExtended } from "../types";
 
 const images = new Hono();
 
-// Cloudflare API details
-const API_URL = "https://api.cloudflare.com/client/v4/accounts/e206e81b43614ccd76aed224ad5e1cbc/images/v1";
-
 images.post('/upload', async (c: ContextExtended) => {
   try {
+    // Cloudflare API details
+    const API_URL = `https://api.cloudflare.com/client/v4/accounts/${c.env.CLOUDFLARE_ACCOUNT_ID}/images/v1`;
     const TOKEN = c.env.CLOUDFLARE_API_TOKEN;
     const body = await c.req.parseBody();
-    console.log(body['file']);
+
     // Validate input
     if (!body.file) {
       return c.json({ error: 'Image file is required' }, 400);
     }
-
-    // Create FormData to send to Cloudflare API
-    // const formData = new FormData();
-    // formData.append('file', form.file, form.file.name);
 
     // Make the API request to Cloudflare
     const response = await fetch(API_URL, {
@@ -28,7 +23,7 @@ images.post('/upload', async (c: ContextExtended) => {
       },
       body: body,
     });
-    console.log('Cloudflare Token:', TOKEN);
+
     const result = await response.json();
 
     if (!response.ok) {
